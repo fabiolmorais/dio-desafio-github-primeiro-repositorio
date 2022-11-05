183db418469a115c405aaaff4ba0009922518266
Fabio L. Morais <fabio.lmorais99@gmail.com>
2022-11-04 21:37:58 -0300

    Pastas inicialmente estruturadas e adicionado as primeiras anotações sobre o módulo 'Primeiros Passos Com JavaScript' + o primeiro desafio realizado em aula

---
+++ primeiros-passos-com-javascript/variaveis-e-operadores/desafio.js
@@ -0,0 +1,7 @@
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaKm = 100;
+
+const custoDaViagem = distanciaKm / kmPorLitros * precoCombustivel;
+
+console.log(custoDaViagem);
